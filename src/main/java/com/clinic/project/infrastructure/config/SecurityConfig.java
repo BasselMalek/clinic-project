bbcc723f0484dab7161ce5a080f474d46317c36d
Bassel W. Ibrahim <bassel.walid04@gmail.com>
2025-05-11 06:19:14 +0300

    fix semiclon

--- src/main/java/com/clinic/project/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/clinic/project/infrastructure/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // Enable CORS
+                .cors(cors -> cors.configurationSource(corsConfigurationSource())); // Enable CORS
         //         .authorizeHttpRequests(auth -> auth
         //                 // Allow unauthenticated access to static content
         //                 .requestMatchers("/static/**", "/public/**", "/resources/**", "/favicon.ico").permitAll()
